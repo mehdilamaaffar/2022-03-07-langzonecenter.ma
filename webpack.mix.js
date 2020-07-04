70515d64538ec1ba3db3d3d3849ca4dc8b7cf0ad
mehdi lamaaffar <mehdilamaafar@gmail.com>
2020-07-04 23:32:54 +0100

    app 0.1

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,24 +13,44 @@ let mix = require('laravel-mix');
 
 mix
     // .js('resources/assets/js/app.js', 'public/js')
-    .sass('scss/style.scss', 'css/')
-    // .scripts([
-    //     'resources/assets/vendor/jquery/jquery.min.js',
-    //     'resources/assets/vendor/scrollreveal/scrollreveal.min.js',
-    //     'resources/assets/vendor/magnific-popup/jquery.magnific-popup.min.js',
-    //     'resources/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
-    //     'resources/assets/vendor/jquery-easing/jquery.easing.min.js',
-    //     'resources/assets/js/app-config.js',
-    // ], 'public/js/vendor.js')
-    // .styles([
-    //     'resources/assets/vendor/bootstrap/css/bootstrap.min.css',
-    //     'resources/assets/vendor/font-awesome/css/font-awesome.min.css',
-    //     'resources/assets/vendor/magnific-popup/magnific-popup.css',
-    // ], 'public/css/vendor.css')
+    .sass('scss/style.scss', 'css')
+    .scripts([
+        'js/vendor/modernizr-3.5.0.min.js',
+        'js/vendor/jquery-1.12.4.min.js',
+        'js/popper.min.js',
+        'js/bootstrap.min.js',
+        'js/owl.carousel.min.js',
+        'js/isotope.pkgd.min.js',
+        'js/ajax-form.js',
+        'js/waypoints.min.js',
+        'js/jquery.counterup.min.js',
+        'js/imagesloaded.pkgd.min.js',
+        'js/scrollIt.js',
+        'js/jquery.scrollUp.min.js',
+        'js/wow.min.js',
+        'js/nice-select.min.js',
+        'js/jquery.slicknav.min.js',
+        'js/jquery.magnific-popup.min.js',
+        'js/plugins.js',
+        'js/gijgo.min.js',
+        'js/contact.js',
+        'js/jquery.ajaxchimp.min.js',
+        'js/jquery.form.js',
+        'js/jquery.validate.min.js',
+        'js/mail-script.js',
+        'js/main.js'
+    ], 'js/app.js')
     .browserSync({
         proxy: 'localhost:8000',
         open: false,
+        files: ['css/**/*.css', '*.html'],
     })
     .options({
         processCssUrls: false,
     })
+    .styles([
+        'resources/assets/vendor/bootstrap/css/bootstrap.min.css',
+        'resources/assets/vendor/font-awesome/css/font-awesome.min.css',
+        'resources/assets/vendor/magnific-popup/magnific-popup.css',
+    ], 'public/css/vendor.css')
+    .minify('js/app.js')
